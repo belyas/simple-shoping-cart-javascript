67eb0e14a3003b4ea6ddd0044350f7e031acb0dc
yassine belkaid <yassine.belkaid87@gmail.com>
2019-05-25 16:08:42 +0000

    added close sidedrawer functionality

--- app.js
+++ app.js
@@ -4,11 +4,16 @@
     const cartDOM = document.querySelector('.cart');
     const closeCartBtn = document.querySelector('.close-cart');
 
-    cartBtn.addEventListener('click', showCart);
-    closeCartBtn.addEventListener('click', hideCart);
-
-    function showCart() {
+    const showCart = () => {
         cartOverlay.classList.add('transparentBcg');
         cartDOM.classList.add('showCart');
-    }
+    };
+
+    const hideCart = () => {
+        cartOverlay.classList.remove('transparentBcg');
+        cartDOM.classList.remove('showCart');
+    };
+
+    cartBtn.addEventListener('click', showCart);
+    closeCartBtn.addEventListener('click', hideCart);
 })();
