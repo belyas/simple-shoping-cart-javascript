53fb018bb24fe46227088d30312f63ad2dfe14ba
yassine belkaid <yassine.belkaid87@gmail.com>
2019-05-25 15:57:31 +0000

    changed logo

--- app.js
+++ app.js
@@ -2,8 +2,10 @@
     const cartBtn = document.querySelector('.cart-btn');
     const cartOverlay = document.querySelector('.cart-overlay');
     const cartDOM = document.querySelector('.cart');
+    const closeCartBtn = document.querySelector('.close-cart');
 
     cartBtn.addEventListener('click', showCart);
+    closeCartBtn.addEventListener('click', hideCart);
 
     function showCart() {
         cartOverlay.classList.add('transparentBcg');
