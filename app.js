1c75f4945217fd1c336bd85dd5beb22bef068a6a
yassine belkaid <yassine.belkaid87@gmail.com>
2019-05-26 15:34:45 +0000

    added products dynamically from json file

--- app.js
+++ app.js
@@ -4,6 +4,18 @@
     const cartDOM = document.querySelector('.cart');
     const closeCartBtn = document.querySelector('.close-cart');
 
+    const formatPrice = (price, currency = '$') => {
+        let formattedPrice = price;
+
+        switch (currency) {
+            case '$':
+                formattedPrice = `${currency}${price}`;
+                break;
+        }
+
+        return formattedPrice;
+    };
+
     const showCart = () => {
         cartOverlay.classList.add('transparentBcg');
         cartDOM.classList.add('showCart');
@@ -13,6 +25,49 @@
         cartOverlay.classList.remove('transparentBcg');
         cartDOM.classList.remove('showCart');
     };
+
+    const loadProductsFromJson = async () => {
+        const result = await fetch('products.json');
+        const products = await result.json();
+
+        return products;
+    };
+
+    const productGridLayout = product => formatter => {
+        return `<article class="product">
+            <div class="img-container">
+                <img
+                    src="${product.image_url}"
+                    alt="${product.title}"
+                    class="product-img"
+                />
+                <button class="bag-btn" data-id="${product.id}">
+                    <i
+                        class="fa fa-shopping-cart"
+                        aria-hidden="true"
+                    ></i>
+                    add to cart
+                </button>
+            </div>
+            <h3>${product.title}</h3>
+            <h4>${formatter(product.price)}</h4>
+        </article>`;
+    };
+
+    const createProductsGrids = loadProductsfunc => async formatter => {
+        const productsContainer = document.getElementById('products-container');
+        const products = await loadProductsfunc();
+        let _html = '';
+
+        products.items.forEach(product => {
+            _html += productGridLayout(product)(formatter);
+        });
+
+        productsContainer.innerHTML = _html;
+    };
+
+    // Display products from json file
+    createProductsGrids(loadProductsFromJson)(formatPrice);
 
     cartBtn.addEventListener('click', showCart);
     closeCartBtn.addEventListener('click', hideCart);
